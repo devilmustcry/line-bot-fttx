4c849318a97fa0246c9dfc220fe7933243954981
Arut Thanomwatana <arut.tha@gmail.com>
2018-09-19 17:55:09 +0700

    Change to express for easy dev

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
-const Koa = require('koa');
-const app = new Koa();
+const express = require('express')
+const port = process.env.PORT || 5000
+const line = require('@line/bot-sdk');
 
-app.use(async ctx => {
-  ctx.body = 'Hello World';
+const config = {
+  channelAccessToken: 'oCV3ANut0xcuUWbWtaOjid5IqBtkgcdY7GL6bEXL4aZMhFjXHKw2iQHzcLYbk9tgzD61mVb67W9ap315frqdzcNqJBdltEAWT9DB0ozP6zdM4zfRqKWYGk9BqeMkwENjwVhPY4b3knvBYnynr+yY5gdB04t89/1O/w1cDnyilFU=',
+  channelSecret: '1d80d885eabef21974c85038a7845c6a'
+}
+
+const app = express()
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>')
+})
+app.post('/webhook', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result));
 });
+const client = new line.Client(config);
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return Promise.resolve(null);
+  }
 
-app.listen(process.env.PORT || 5000);
-console.log('*Server is running at localhost:5000')
+  return client.replyMessage(event.replyToken, {
+    type: 'text',
+    text: event.message.text
+  });
+}
+app.listen(port, () => console.log(`app listening on port ${port}!`))
+
+
