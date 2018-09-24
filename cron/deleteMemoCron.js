b83f4cd76455d29601d7b2b229eeb00174f09194
Arut Thanomwatana <arut.tha@gmail.com>
2018-09-24 18:07:09 +0700

    Finish function delcron and noticron everyday

--- cron/deleteMemoCron.js
+++ cron/deleteMemoCron.js
@@ -4,9 +4,8 @@ const memo = require('../services/memo')
 const CronJob = cron.CronJob
 
 module.exports = new CronJob({
-  cronTime: '0 0 1 * * *',
-  runOnInit: true,
-  // run everyday time 23.30
+  cronTime: '0 30 0 * * *',
+  // run everyday time 6:30
   async onTick () {
     try {
       await memo.deleteOutOfDateMeeting()
--- cron/memoCron.js
+++ cron/memoCron.js
@@ -6,7 +6,6 @@ const CronJob = cron.CronJob
 
 module.exports = new CronJob({
   cronTime: '0 0 1 * * *',
-  runOnInit: true,
   // run everyday time 23.30
   async onTick () {
     try {
